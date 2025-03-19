d8c08cac0571a8d7b0c9c590e78d90c8678f7b48
gFerreira95 <gilsonferreiradepaula@gmail.com>
2025-03-19 19:19:52 -0300

    1 - user routes and controllers added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/profile/:username', getUserProfile)
-router.get('/suggested', getUserProfile)
-router.post('/follow/:id', followUnfollowUser)
-router.post('/update', updateUserProfile)
+router.get('/profile/:username', protectRoute, getUserProfile);
+router.get('/suggested', protectRoute, getSuggestedUsers);
+router.post('/follow/:id',protectRoute, followUnfollowUser);
+router.post('/update', protectRoute, updateUser);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,14 @@ import userRoutes from './routes/user.routes.js'
 import dotenv from 'dotenv'
 import connectDb from './db/connectDb.js';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const port = process.env.PORT;
 const app = express();
@@ -13,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 
 app.listen(port, () => {
